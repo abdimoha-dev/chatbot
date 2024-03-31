59b1591e0bcbac3a07f5f6501ef5c7adac2c3578
abdimoha-dev <abdymohammed@gmail.com>
2024-03-31 21:15:59 +0300

    update urls

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-y7wq!=q!@8+ieac^8658a5_&kh9d$#avd!8nkxs-lqfchvfo2e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*','146.190.157.221']
 
 
 # Application definition
